1392065759eaa789f6379f81aa173aef718437c8
hitro11 <rohitnazareth07@gmail.com>
2017-05-18 14:50:46 +0000

    cleaned up contact routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
  root to: 'pages#home'
  get 'about', to: 'pages#about'
- resources :contacts
+ resources :contacts, only: :create
+ get 'contact', to: 'contacts#new', as: 'new_contact'
 end
