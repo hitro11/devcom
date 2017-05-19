dbc088d740a737a557b951b09337f704d852e07d
hitro11 <rohitnazareth07@gmail.com>
2017-05-19 23:20:43 +0000

    Added plans table

---
+++ db/migrate/20170519231133_create_plans.rb
@@ -0,0 +1,6 @@
+class CreatePlans < ActiveRecord::Migration[5.0]
+  def change
+    create_table :plans do |t|
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170519025744) do
+ActiveRecord::Schema.define(version: 20170519231133) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
@@ -18,6 +18,9 @@ ActiveRecord::Schema.define(version: 20170519025744) do
     t.text     "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "plans", force: :cascade do |t|
   end
 
   create_table "users", force: :cascade do |t|
