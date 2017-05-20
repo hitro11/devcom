605348397ac938f9532ab59cf416e54e3625a9a1
hitro11 <rohitnazareth07@gmail.com>
2017-05-20 20:05:54 +0000

    Added plan vars and params to home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,9 @@
 class PagesController < ApplicationController
+  
+  # GET request for home page
   def home
+    @basic_plan = Plan.find(1)
+    @pro_plan = Plan.find(2)
   end
   
   def about
--- db/schema.rb
+++ db/schema.rb
@@ -21,6 +21,10 @@ ActiveRecord::Schema.define(version: 20170519234813) do
   end
 
   create_table "plans", force: :cascade do |t|
+    t.string   "name"
+    t.decimal  "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
