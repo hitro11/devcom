423435e42e63e554d5ef79cad8d465b7fc66cf70
hitro11 <rohitnazareth07@gmail.com>
2017-05-21 10:12:37 +0000

    Added Stripe JS library and pro form id

--- db/migrate/20170521093802_add_stripe_customer_token_to_users.rb
+++ db/migrate/20170521093802_add_stripe_customer_token_to_users.rb
@@ -1,5 +1,5 @@
 class AddStripeCustomerTokenToUsers < ActiveRecord::Migration[5.0]
   def change
-     add_column :users, :stripe_customer_token, :string
+    add_column :users, :stripe_customer_token, :string
   end
 end
